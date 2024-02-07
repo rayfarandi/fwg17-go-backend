591efdf6c10e151f10b0da7afcb98382eea968a5
Ran Ran <rayfarandi1994@gmail.com>
2024-02-07 14:58:48 +0700

    change port & nodemone

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.6
 
 require (
 	github.com/KEINOS/go-argonize v1.1.0
+	github.com/LukaGiorgadze/gonull v1.2.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rayfarandi/fwg17-go-backend/src/routers"
+	"github.com/rayfarandi/fwg17-go-backend/src/services"
 )
 
 func main() {
 	r := gin.Default()
 	routers.Combine(r)
-	r.Run()
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, &services.Response{
+			Success: false,
+			Message: "Resource not found",
+		})
+	})
+	r.Run("127.0.0.1:8888")
 }
